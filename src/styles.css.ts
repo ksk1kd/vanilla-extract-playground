9f198e720b56e8a02560ae23deffb204d074e680
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-19 00:06:36 +0900

    add global style

--- src/styles.css.ts
+++ src/styles.css.ts
@@ -1,4 +1,8 @@
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
+
+globalStyle('html', {
+  boxSizing: 'border-box',
+})
 
 export const title = style({
   fontSize: '4rem',
