d939b3640de6a4ce88ccabc15b89d7c392f3902d
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-19 00:15:05 +0900

    add css variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { title } from './styles.css'
+import { subtitle, title } from './styles.css'
 
 function App() {
   return (
     <>
       <div className="container">
         <h1 className={title}>Vanilla Extract Playground</h1>
+        <h2 className={subtitle}>Sub Title</h2>
       </div>
     </>
   )
--- src/styles.css.ts
+++ src/styles.css.ts
@@ -2,8 +2,17 @@ import { globalStyle, style } from '@vanilla-extract/css'
 
 globalStyle('html', {
   boxSizing: 'border-box',
+  vars: {
+    '--font-size--small': '1rem',
+    '--font-size--medium': '2rem',
+    '--font-size--large': '3rem',
+  },
 })
 
 export const title = style({
   fontSize: '4rem',
+})
+
+export const subtitle = style({
+  fontSize: 'var(--font-size--large)',
 })
